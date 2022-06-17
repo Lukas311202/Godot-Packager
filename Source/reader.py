a443486a7dbb35fa738bbd23fc72c861018d6983
Lukas Marusenko <63213730+Lukas311202@users.noreply.github.com>
2022-06-17 17:19:24 +0200

    Update reader.py
    now also searches for .wav files

--- Source/reader.py
+++ Source/reader.py
@@ -1,14 +1,11 @@
-from dataclasses import replace
 from os.path import exists
 import re
-from tokenize import String
-
 # f = open("debuging\World.tscn", "r")
 
 # txt = f.read()
 # f.close()
 
-replacement_path = 'D:\Gravity Falls Game\\'
+REPLACEMENT_PATH = 'D:\Gravity Falls Game\\'
 
 
 #returns the dependencies with their full path
@@ -16,22 +13,24 @@ def get_dependencies_path(txt_file):
     """searches for dependencies in tscn and tres files. DOESN'T Work in Gd files, for that use get_dependencies_path_gd"""
     ext_block = re.findall("ext_resource.*]", txt_file)
     ext_block = "\n".join(ext_block)
-    expression_tscn = 'res:.*tscn'                  #(ext_resource.*)(tscn|gd|png)
+    expression_tscn = 'res:.*tscn'  #(ext_resource.*)(tscn|gd|png)
     expression_gd = 'res:.*gd'
     expression_png = 'res:.*png'
     expression_mp4 = 'res:.*mp4'
     expression_shader = 'res:.*shader'
+    expression_wav = 'res:.*wav'
     dependencies = re.findall(expression_tscn, ext_block)
     dependencies += re.findall(expression_gd, ext_block)
     dependencies += re.findall(expression_png, ext_block)
     dependencies += re.findall(expression_mp4, ext_block)
     dependencies += re.findall(expression_shader, ext_block)
+    dependencies += re.findall(expression_wav, ext_block)
 
     actual_path = []
 
     #need to replace the godot file path with actual windows filepath
     for i in dependencies:
-        actual_path.append(i.replace("res://", replacement_path))
+        actual_path.append(i.replace("res://", REPLACEMENT_PATH))
 
 
     return actual_path
@@ -44,38 +43,34 @@ def get_dependencies_path_gd(txt_file):
     expression_png = 'res:.*png'
     expression_mp4 = 'res:.*mp4'
     expression_shader = 'res:.*shader'
+    expression_wav = 'res:.*wav'
     dependencies = re.findall(expression_tscn, txt_file)
     dependencies += re.findall(expression_gd, txt_file)
     dependencies += re.findall(expression_png, txt_file)
     dependencies += re.findall(expression_mp4, txt_file)
     dependencies += re.findall(expression_shader, txt_file)
+    dependencies += re.findall(expression_wav, txt_file)
 
     actual_path = []
 
     #need to replace the godot file path with actual windows filepath
     for i in dependencies:
-        actual_path.append(i.replace("res://", replacement_path))
+        actual_path.append(i.replace("res://", REPLACEMENT_PATH))
 
 
     return actual_path
 
-    pass
-
 def collect_dependencies(scene_path):
     """reads a tscn or gd file for external dependencies and then returns a list with all dependencies filepaths"""
-    
-
     #print("read ", scene_path)
-    if not exists(scene_path) or "png" in scene_path or "shader" in scene_path or "mp4" in scene_path:
-        return
-        
+    if not exists(scene_path) or "png" in scene_path or "shader" in scene_path or "mp4" in scene_path or "wav" in scene_path:
+        return     
     sub_dependencies = []
     all_paths = []
     #first read the scene path to extract required dependencies
-    f = open(scene_path, "r")
+    f = open(scene_path, "r", encoding=None)
     txt = f.read()
     f.close()
-    
     if ".gd" in scene_path:
         dependencies = get_dependencies_path_gd(txt)
     else:
